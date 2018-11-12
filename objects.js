8e9a383dab0ba1afc9c5cbfb5b974a067b22e69a
Claire Nguyen <disarrangedorder@gmail.com>
2018-11-12 06:54:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ var playlist = {
   Madonna: "Like a Prayer",
   Bonobo: "Second Sun"
 };
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist.artistName(songTitle)
+  return playlist
+}
